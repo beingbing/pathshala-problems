29531417ba719f8216f6cac939f0d1d4b16b8272
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-12 01:37:36 +0530

    arrays | assignment 2 | 03 | reverse lookup 2-D

---
+++ geeksforgeeks/03.cpp
@@ -0,0 +1,32 @@
+// C++ program to find the sum of all
+// possible submatrices of a given Matrix
+
+#include <iostream>
+#define n 3
+using namespace std;
+
+// Function to find the sum of all
+// possible submatrices of a given Matrix
+int matrixSum(int arr[][n]) {
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+			// Number of ways to choose
+			// from top-left elements
+			int top_left = (i + 1) * (j + 1);
+			// Number of ways to choose
+			// from bottom-right elements
+			int bottom_right = (n - i) * (n - j);
+			sum += (top_left * bottom_right * arr[i][j]);
+		}
+	return sum;
+}
+
+// Driver Code
+int main() {
+	int arr[][n] = { { 1, 1, 1 },
+					{ 1, 1, 1 },
+					{ 1, 1, 1 } };
+	cout << matrixSum(arr);
+	return 0;
+}
