1dfb33e6e468195faae0452b710139af9b1cb25e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-13 11:01:07 +0530

    arrays | homework 2 | 02 | sliding window technique

---
+++ leetcode/0567.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> s1chars(26), s2chars(26);
+        for (auto alps : s1) s1chars[alps-'a']++
+        int k = s1.size(), n = s2.size();
+        for (int i{0}; i<k; i++) s2chars[s2[i]-'a']++;
+        for (int i{k}; i<n; i++) {
+            if (s1chars == s2chars) return true;
+            s2chars[s2[i]-'a']++;
+            s2chars[s2[i-k]-'a']--;
+        }
+        if (s1chars == s2chars) return true;
+        return false;
+    }
+};
+
+int main() {
+    Solution* obj = new Solution();
+    cout << obj->checkInclusion("ab", "eidbaooo") << endl;
+    return 0;
+}
