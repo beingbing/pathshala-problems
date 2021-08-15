f0f5e04334eef4e8a6daea011b6a2ec73c02b063
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-15 11:59:21 +0530

    arrays | homework 3 | 03

---
+++ 0169.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ele = nums[0], freq = 1, n = nums.size();
+        for (int i{1}; i<n; i++) {
+            if (ele == nums[i]) freq++;
+            else freq--;
+            if (freq == 0) {ele = nums[i];freq = 1;}
+        }
+        return ele;
+    }
+};
