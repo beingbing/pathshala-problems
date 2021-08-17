8427e3cc8eb50656f0cbafa8ec4e8c63c733eb6d
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-17 14:06:20 +0530

    mathematics | homework 1 | 02

---
+++ leetcode/0914.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+    
+    int findGCDM2(int a, int b) {
+        int x = min(a, b);
+        int y = max(a, b);
+        if (x == 0) return y;
+        if (y%x == 0) return x;
+        return findGCDM2(x, y%x);
+    }
+    
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int, int> mp;
+        for (auto ele : deck) {
+            if (mp.find(ele) == mp.end()) mp.insert({ele, 1});
+            else mp[ele]++;
+        }
+        vector<int> vec;
+        for (auto kvp : mp) vec.push_back(kvp.second);
+        int gcd = vec[0];
+        int n = vec.size();
+        for (int i{1}; i<n; i++) gcd = findGCDM2(gcd, vec[i]);
+        if (gcd >= 2) return true;
+        return false;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,2,3,4,4,3,2,1};
+    Solution* obj = new Solution();
+    cout << obj->hasGroupsSizeX(nums) << endl;
+    return 0;
+}
