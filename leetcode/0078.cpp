d78e27e8633f3a84d165fc21a0176f2688f69c98
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-21 16:45:00 +0530

    recursion | assignment 1 | 02

--- leetcode/0078.cpp
+++ leetcode/0078.cpp
@@ -22,6 +22,16 @@ public:
         subsets(nums, 0, sub, subs);
         return subs;
     }
+
+    // using bit manipulation
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> ans(p);
+        for (int i = 0; i < p; i++)
+            for (int j = 0; j < n; j++)
+                if ((i >> j) & 1) ans[i].push_back(nums[j]);
+        return ans;
+    }
 };
 
 int main() {
