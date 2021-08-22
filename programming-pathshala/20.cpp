18a174db9dd946422186c01c2c97801ece7729ec
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-08-22 13:38:35 +0530

    backtracking | lecture 4

---
+++ programming-pathshala/20.cpp
@@ -0,0 +1,34 @@
+// print all the valid paths that a rat can take in a maze to 
+// reach it's destination.
+#include <iostream>
+#include <vector>
+using namespace std;
+    vector<vector<pair<int, int>>> ans;
+
+    void findPaths(vector<vector<int>>& maze, vector<pair<int, int>>& route, int i, int j) {
+        if (maze[i][j] == 1) return;
+        if (i == j and j == maze.size()-1) {
+            route.push_back({i,j});
+            ans.push_back(route);
+            route.clear();
+            return;
+        }
+        route.push_back({i,j});
+        if (i == maze.size()-1) findPaths(maze, route, i, j+1);
+        else if (j == maze[0].size()-1) findPaths(maze, route, i+1, j);
+        else {
+            findPaths(maze, route, i, j+1);
+            findPaths(maze, route, i+1, j);
+        }
+    }
+
+int main() {
+    vector<vector<int>> maze = {{0,0,1,1},{0,0,1,1},{1,0,1,0},{0,0,0,0}};
+    vector<pair<int, int>> route;
+    findPaths(maze, route, 0, 0);
+    for (auto path : ans) {
+        for (auto cell : path) cout << "(" << cell.first << ", " << cell.second << ")\t";
+        cout << endl;
+    }
+    return 0;
+}
