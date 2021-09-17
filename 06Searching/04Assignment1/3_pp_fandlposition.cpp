785f8e22eb6c1e2c1d30d58c43c162d8df08fc86
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-09-17 07:59:36 +0530

    searching | assignment 1 3/4

---
+++ 06Searching/04Assignment1/3_pp_fandlposition.cpp
@@ -0,0 +1,75 @@
+/*
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a
+given target value.
+If target is not found in the array, return −1.
+You must write an algorithm with O(log(n)) runtime complexity.
+
+Input Format
+The first line contains an integer N, number of elements in the array nums
+The second line contains N space-separated integers nums[1],......, nums[N]
+The third line of the input contains a single integer T denoting the number of test cases.
+The description of T test cases follows.
+Each test case consists of a positive integer target
+
+Constraints
+1 ≤ N ≤ 10^5
+1 ≤ nums[i], target ≤ 10^9
+1 ≤ T ≤ 10^5
+
+Output Format
+For each test case, print starting and ending index of the target value or −1
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstPosition(vector<int> &nums, int &n, int &target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > target) high = mid-1;
+        else if (nums[mid] < target) low = mid+1;
+        else {
+            if (mid == 0) return mid;
+            else if (nums[mid-1] != target) return mid;
+            else high = mid-1;
+        }
+    }
+    return -1;
+}
+
+int lastPosition(vector<int> &nums, int &n, int &target) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > target) high = mid-1;
+        else if (nums[mid] < target) low = mid+1;
+        else {
+            if (mid == n-1) return mid;
+            else if (nums[mid+1] != target) return mid;
+            else low = mid+1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i{0}; i<n; i++) cin >> vec[i];
+    int t;
+    cin >> t;
+    while (t--) {
+        int target;
+        cin >> target;
+        int frst = firstPosition(vec, n, target);
+        int last = lastPosition(vec, n, target);
+        if (frst == last && last == -1) cout << last << endl;
+        else cout << frst+1 << " " << last+1 << endl;
+    }
+    return 0;
+}
