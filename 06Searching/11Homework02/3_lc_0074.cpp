ca89c1f6020b7f87a79dc1d91ed68db8e3d6fcdb
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-09-19 10:18:14 +0530

    searching | homework 2 3/4

---
+++ 06Searching/11Homework02/3_lc_0074.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (!matrix.size()) return false;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int lo = 0;
+        int hi = m*n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if (matrix[mid/m][mid%m] == target) return true;
+            else if (matrix[mid/m][mid%m] < target) lo = mid+1;
+            else hi = mid-1;
+        }
+        return false;
+    }
+};
