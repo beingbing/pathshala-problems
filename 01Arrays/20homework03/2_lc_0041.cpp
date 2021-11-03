9de5059852d5bed1165fea81857ce31410f3c56e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-11-03 17:41:12 +0530

    arrays: revision | homework 3 problem 2

--- 01Arrays/20homework03/2_lc_0041.cpp
+++ 01Arrays/20homework03/2_lc_0041.cpp
@@ -7,17 +7,9 @@ public:
     int firstMissingPositive(vector<int>& a) {
         int n = a.size();
         for (int i{0}; i<n; i++)
-            if (a[i] >0 and a[i]<=n) {
-                int idx = a[i];
-                while (idx > 0 and idx <= n and a[idx-1] != idx) {
-                    int temp = a[idx-1];
-                    a[idx-1] = idx;
-                    idx = temp;
-                    
-                }
-            }
-        int i{0};
-        for (; i<n; i++) if (a[i] != i+1) return i+1;
+            while (a[i] > 0 and a[i] <= n and a[a[i]-1] != a[i])
+                swap(a[a[i]-1], a[i]);
+        for (int i{0}; i<n; i++) if (a[i] != i+1) return i+1;
         return n+1;
     }
 };
