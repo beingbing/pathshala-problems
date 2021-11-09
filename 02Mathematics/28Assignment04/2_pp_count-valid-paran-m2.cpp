0520db1d38a3521254cb4758afc8c071b0d3dd2b
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-11-09 16:47:09 +0530

    mathematics: revision | assignment 4 problem 2

--- 02Mathematics/28Assignment04/2_pp_count-valid-paran-m2.cpp
+++ 02Mathematics/28Assignment04/2_pp_count-valid-paran-m2.cpp
@@ -1,29 +1,3 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// int main() {
-//     unsigned long long int t;
-//     cin >> t;
-//     while (t--) {
-//         unsigned long long int n;
-//         cin >> n;
-//         if (n%2 != 0) {
-//             cout << -1 << endl;
-//             continue;
-//         }
-//          n /= 2;
-//         vector<unsigned long long int> cnt(n+1, 0);
-//         cnt[0] = cnt[1] = 1;
-//         unsigned long long int m{1000000007};
-//         for (unsigned long long int i{2}; i<=n; i++)
-//             for (unsigned long long int j{0}; j<i; j++)
-//                 cnt[i] += ((cnt[j]%m)*(cnt[i-j-1]%m))%m;
-//         cout << cnt[n] << endl;
-//     }
-//     return 0;
-// }
-
 #include <iostream>
 #include <vector>
 using namespace std;
