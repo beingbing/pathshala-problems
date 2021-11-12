be290e13b4e6955a283aadd1f14f07da795023be
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-11-12 11:30:46 +0530

    recursion | homework 1 problem 5

--- 04Recursion/14Homework01/5_cf_676b.cpp
+++ 04Recursion/14Homework01/5_cf_676b.cpp
@@ -2,13 +2,24 @@
 #include <vector>
 using namespace std;
 
-int fillGlasses(int n, int t) {
-    return 0;
+void fillGlasses(vector<vector<double>>& mat, int& n, int i, int j, double k) {
+    if (i >= n || j >= n || j > i) return;
+    mat[i][j] += k;
+    if (mat[i][j] > 1) {
+        double xtra = mat[i][j] - 1;
+        mat[i][j] -= xtra;
+        fillGlasses(mat, n, i+1, j, xtra*0.5);
+        fillGlasses(mat, n, i+1, j+1, xtra*0.5);
+    }
 }
 
 int main() {
     int n,t;
     cin >> n >> t;
-    cout << fillGlasses(n, t) << endl;
+    vector<vector<double>> mat(n, vector<double>(n, 0));
+    int cnt = 0, tme = 0;
+    for (int i{0}; i<t; i++) fillGlasses(mat, n, 0, 0, 1);
+    for (int i{0}; i<n; i++) for (int j{0}; j<=i; j++) if (mat[i][j] == 1) cnt++;
+    cout << cnt << endl;
     return 0;
 }
