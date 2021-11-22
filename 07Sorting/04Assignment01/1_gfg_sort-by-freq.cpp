4ce0129cc96a44408ca5aadb8cc60b803f70b2d5
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-11-23 04:43:12 +0530

    sorting | assignment 1 1/3 M2

--- 07Sorting/04Assignment01/1_gfg_sort-by-freq.cpp
+++ 07Sorting/04Assignment01/1_gfg_sort-by-freq.cpp
@@ -1,7 +1,56 @@
-#include <algorithm>
+// method 1 (inline comparator)
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+//         vector<int> vec(n,0);
+//         for (int i{0}; i<n; i++) cin >> vec[i];
+//         vector<int> freq(61,0);
+//         for (auto ele : vec) freq[ele]++;
+//         sort(vec.begin(), vec.end(), [&](int x, int y) {
+//             if (freq[x] == freq[y]) return x < y;
+//             return freq[x] > freq[y];
+//         });
+//         for (auto ele : vec) cout << ele << " ";
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
+
+// method 2 (comparator with class/struct)
 #include <iostream>
 #include <vector>
 using namespace std;
+
+struct triplet {
+    int val, freq, idx;
+};
+
+bool cmpr(triplet t1, triplet t2) {
+    if (t1.freq != t2.freq) return t1.freq > t2.freq;
+    if (t1.val != t2.val) return t1.val < t2.val;
+    return t1.idx < t2.idx;
+}
+
+vector<triplet> constructTriplet(int& n, vector<int>& vec, vector<int>& freq) {
+    vector<triplet> v;
+    for (int i = 0; i < n; i++) {
+        triplet tri;
+        tri.val = vec[i];
+        tri.freq = freq[vec[i]];
+        tri.idx = i;
+        v.push_back(tri);
+    }
+    return v;
+}
 
 int main() {
     int t;
@@ -9,17 +58,15 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<int> vec(n,0);
-        for (int i{0}; i<n; i++) cin >> vec[i];
-        vector<int> freq(61,0);
-        for (auto ele : vec) freq[ele]++;
-        sort(vec.begin(), vec.end(), [&](int x, int y) {
-            if (freq[x] == freq[y]) return x < y;
-            return freq[x] > freq[y];
-        });
-        for (auto ele : vec) cout << ele << " ";
+        vector<int> vec(n, 0), freq(61, 0);
+        for (int i = 0; i < n; i++) {
+            cin >> vec[i];
+            freq[vec[i]]++;
+        }
+        vector<triplet> v = constructTriplet(n, vec, freq);
+        sort(v.begin(), v.end(), cmpr);
+        for (auto ele : v) cout << ele.val << " ";
         cout << endl;
     }
-    
     return 0;
 }
