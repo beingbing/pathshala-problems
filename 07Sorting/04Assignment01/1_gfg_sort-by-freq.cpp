4e484ade82dc192500df39aa4decf50f3bd2a1d0
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-11-23 06:29:28 +0530

    sorting | homework 1 3/6

--- 07Sorting/04Assignment01/1_gfg_sort-by-freq.cpp
+++ 07Sorting/04Assignment01/1_gfg_sort-by-freq.cpp
@@ -1,31 +1,3 @@
-// method 1 (inline comparator)
-// #include <algorithm>
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// int main() {
-//     int t;
-//     cin >> t;
-//     while (t--) {
-//         int n;
-//         cin >> n;
-//         vector<int> vec(n,0);
-//         for (int i{0}; i<n; i++) cin >> vec[i];
-//         vector<int> freq(61,0);
-//         for (auto ele : vec) freq[ele]++;
-//         sort(vec.begin(), vec.end(), [&](int x, int y) {
-//             if (freq[x] == freq[y]) return x < y;
-//             return freq[x] > freq[y];
-//         });
-//         for (auto ele : vec) cout << ele << " ";
-//         cout << endl;
-//     }
-    
-//     return 0;
-// }
-
-// method 2 (comparator with class/struct)
 #include <iostream>
 #include <vector>
 using namespace std;
