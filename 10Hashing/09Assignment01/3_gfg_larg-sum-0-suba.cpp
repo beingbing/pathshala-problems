c3bce5e4aa214c1f443fdfb39872bddd82eeab57
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-12-12 17:10:00 +0530

    hashing | assignment 1 3/5

---
+++ 10Hashing/09Assignment01/3_gfg_larg-sum-0-suba.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int maxLen(int arr[], int n) {
+    unordered_map<int, int> presum;
+    int sum = 0;
+    int max_len = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        if (arr[i] == 0 && max_len == 0) max_len = 1;
+        if (sum == 0) max_len = i + 1;
+        if (presum.find(sum) != presum.end()) max_len = max(max_len, i - presum[sum]);
+        else presum[sum] = i;
+    }
+    return max_len;
+}
+
+int main() {
+    int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    int n = 8;
+    cout << "Length of the longest 0 sum subarray is " << maxLen(arr, n);
+    return 0;
+}
