c2132d1f48105696ed7217239ce0b9375a3c6673
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-12-15 12:05:13 +0530

    hashing: revision | assignment 2 Q3

--- 10Hashing/14Assignment02/3_lc_0128.cpp
+++ 10Hashing/14Assignment02/3_lc_0128.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <set>
 #include <unordered_map>
 #include <vector>
 using namespace std;
 
 class Solution {
     public:
-    int longestConsecutive(vector<int>& a) {
+    int longestConsecutiveM1(vector<int>& a) {
         unordered_map<int, int> m;
         int n = a.size(), ans = 0;
         for (auto ele : a) {
@@ -20,11 +21,46 @@ class Solution {
         }
         return ans;
     }
+
+    int longestConsecutiveM2(vector<int>& a) {
+        int n = a.size();
+        unordered_map<int, int> m, visited;
+        for (int i{0}; i<n; i++) m[a[i]] = 1;
+        int ans = 0;
+        for (int i{0}; i<n; i++) {
+            if (m.find(a[i]-1) != m.end()) continue;
+            if (visited.find(a[i]) != visited.end()) continue;
+            visited[a[i]] = 1;
+            int len = 0;
+            int x = a[i];
+            while (m.find(x) != m.end()) {
+                len++;
+                x++;
+            }
+            ans = max(ans, len);
+        }
+        return ans;
+    }
+
+    int longestConsecutiveM3(vector<int>& a) {
+        set<int> s;
+        for(auto ele : a) s.insert(ele);
+        if (s.size() == 1) return 1;
+        int curr_run{1}, mx{0};
+        for(auto sit = s.begin(); sit != s.end(); sit++) {
+            auto it = next(sit, 1);
+            if (it != s.end())
+                if ( (*sit) + 1 == *it ) curr_run++;
+                else curr_run = 1;
+                mx = max(mx, curr_run);
+        }
+        return mx;
+    }
 };
 
 int main() {
     vector<int> nums = {0,3,7,2,5,8,4,6,0,1};
     Solution obj;
-    cout << obj.longestConsecutive(nums) << endl;
+    cout << obj.longestConsecutiveM2(nums) << endl;
     return 0;
 }
