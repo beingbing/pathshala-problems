abaf57d8072f0de11169e76b6e5bde105ca7b405
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-12-21 10:35:20 +0530

    stacks | homework 1 5/5

---
+++ 11Stacks/06Homework01/5_lc_0456.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool find132pattern(vector<int> &nums) {
+        int s3 = INT_MIN;
+        stack<int> st;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] < s3) return true;
+            else while (!st.empty() && nums[i] > st.top()) {
+                    s3 = st.top();
+                    st.pop();
+                }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 1, 4, 2};
+    Solution obj;
+    cout << obj.find132pattern(nums) << endl;
+    return 0;
+}
