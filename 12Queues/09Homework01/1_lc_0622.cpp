b8dfca0bfa45823eb5c2691c5f465d41902a7edc
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-12-25 15:44:33 +0530

    queues | homework 1 1/3

---
+++ 12Queues/09Homework01/1_lc_0622.cpp
@@ -0,0 +1,50 @@
+#include <queue>
+using namespace std;
+
+class MyCircularQueue {
+    
+    vector<int> data;
+    int head, tail, cnt, sz;
+    
+public:
+    MyCircularQueue(int k) {
+        data.resize(k);
+        head = 0;
+        tail = 0;
+        cnt = 0;
+        sz = k;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        data[tail] = value;
+        tail = (tail + 1) % sz;
+        cnt++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        head = (head + 1) % sz;
+        cnt--;
+        return true;
+    }
+    
+    int Front() {
+        if (isEmpty()) return -1;
+        return data[head];
+    }
+    
+    int Rear() {
+        if (isEmpty()) return -1;
+        return data[(tail + sz - 1) % sz];
+    }
+    
+    bool isEmpty() {
+        return cnt == 0;
+    }
+    
+    bool isFull() {
+        return cnt == sz;
+    }
+};
