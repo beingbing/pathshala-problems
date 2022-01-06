c065fcc3e781cb26eb98ee654677cd0f7aaf1cb4
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-06 08:07:16 +0530

    binary trees | assignment 5 4/6

---
+++ 14BinaryTrees/29Assignment05/2_lc_0144.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root) return ans;
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
+            ans.push_back(node->val);
+            if (node->right) stk.push(node->right);
+            if (node->left) stk.push(node->left);
+        }
+        return ans;
+    }
+};
