5cef153f46f9aa8404a78ed1f50c6d60960f7131
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-07 21:30:38 +0530

    binary trees | assignment 6 5/6

---
+++ 14BinaryTrees/34Assignment06/5_lc_0116_m2.cpp
@@ -0,0 +1,39 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+public:
+    Node *connect(Node *root) {
+        if (!root) return root;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                Node* n = q.front();
+                q.pop();
+                if (sz) n->next = q.front();
+                else n->next = NULL;
+                if (n->left) q.push(n->left);
+                if (n->right) q.push(n->right);
+            }
+        }
+        return root;
+    }
+};
