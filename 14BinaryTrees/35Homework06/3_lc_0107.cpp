eedbb061d4e652c8a7b2ff237aa6574081e87d57
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-07 23:02:23 +0530

    binary trees | homework 6 3/4

---
+++ 14BinaryTrees/35Homework06/3_lc_0107.cpp
@@ -0,0 +1,37 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    vector<vector<int>> ans;
+    
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            vector<int> row;
+            int sz = q.size();
+            while (sz--) {
+                TreeNode* n = q.front();
+                q.pop();
+                row.push_back(n->val);
+                if (n->left) q.push(n->left);
+                if (n->right) q.push(n->right);
+            }
+            ans.push_back(row);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
