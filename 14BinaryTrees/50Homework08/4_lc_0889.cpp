5526398d810543611f27634e840c03a3ad9c63d8
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-10 15:56:17 +0530

    binary trees | homework 8 4/5

---
+++ 14BinaryTrees/50Homework08/4_lc_0889.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    int preIndex = 0, posIndex = 0;
+public:
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        TreeNode* root = new TreeNode(pre[preIndex++]);
+        if (root->val != post[posIndex])
+            root->left = constructFromPrePost(pre, post);
+        if (root->val != post[posIndex])
+            root->right = constructFromPrePost(pre, post);
+        posIndex++;
+        return root;
+    }
+};
