30d51168d317f25de697d6cea7157911348c97f0
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-10 06:09:58 +0530

    binary trees | assignment 8 5/5

---
+++ 14BinaryTrees/49Assignment08/5_lc_0652.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    
+    string DFS(TreeNode* root, unordered_map<string, int>& m, vector<TreeNode*>& res){
+        if(!root) return "";
+        string s = to_string(root->val) + "," + DFS(root->left, m, res) + "," + DFS(root->right, m, res);
+        if(++m[s] == 2) res.push_back(root);
+        return s;
+    }
+
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int>m;
+        vector<TreeNode*>res;
+        DFS(root, m, res);
+        return res;
+    }
+};
