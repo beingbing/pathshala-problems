2cfc56755c8bfde64bc180c7acc0c8533d94be23
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-11 15:45:14 +0530

    binary trees | assignment 10 1/2

---
+++ 14BinaryTrees/57Assignment10/1_lc_0236.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return NULL;
+        if (root->val == p->val || root->val == q->val) return root;
+        TreeNode* lst = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rst = lowestCommonAncestor(root->right, p, q);
+        if (!lst) return rst;
+        if (!rst) return lst;
+        return root;
+    }
+};
