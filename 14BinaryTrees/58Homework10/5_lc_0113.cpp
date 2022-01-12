cf0eaa5a197c2514bd3e55b7d7d32f4781a51aa2
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-12 17:59:56 +0530

    binary trees | homework 10 5/6

---
+++ 14BinaryTrees/58Homework10/5_lc_0113.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<int> path;
+        currStatus(root, path, sum);
+        return ans;
+    }
+    
+    void currStatus(TreeNode* root, vector<int> &path, int sum) {
+        if (!root) return;
+        path.push_back(root->val);
+        if (sum - root->val == 0 && !root->left && !root->right) ans.push_back(path);
+        currStatus(root->left, path, sum - root->val);
+        currStatus(root->right, path, sum - root->val);
+        path.pop_back();
+    }
+};
