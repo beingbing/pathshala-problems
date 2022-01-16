3a65f2adad0d50b95fb8f0380ca148c0dcd17b1e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-01-17 01:52:31 +0530

    tries | assignment 1 1/4

---
+++ 18Tries/07Assignment01/1_lc_0208.cpp
@@ -0,0 +1,57 @@
+#include <string>
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isStrPresent{false};
+    int subTreeStrCnt{0};
+
+    TrieNode() {
+        for (int i{0}; i<26; i++) children[i] = nullptr;
+        isStrPresent = false;
+        subTreeStrCnt = 0;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+    void insert(TrieNode* cur, string& word, int pos) {
+        if (pos == word.size()) {
+            cur->isStrPresent = true;
+            return;
+        }
+        cur->subTreeStrCnt++;
+        if (!cur->children[word[pos]-'a']) cur->children[word[pos]-'a'] = new TrieNode();
+        insert(cur->children[word[pos]-'a'], word, pos+1);
+    }
+
+    bool search(TrieNode* cur, string prfx, int pos) {
+        if (!cur) return false;
+        if (pos == prfx.size()) return cur->isStrPresent;
+        return search(cur->children[prfx[pos]-'a'], prfx, pos+1);
+    }
+
+    bool startsWith(TrieNode *cur, string &prfx, int pos) {
+        if (!cur) return false;
+        if (pos == prfx.size()) return true;
+        return startsWith(cur->children[prfx[pos]-'a'], prfx, pos+1);
+    }
+
+public:
+
+    Trie(): root{new TrieNode()} {}
+    
+    void insert(string word) {
+        insert(root, word, 0);
+    }
+    
+    bool search(string word) {
+        return search(root, word, 0);
+    }
+    
+    bool startsWith(string prefix) {
+        return startsWith(root, prefix, 0);
+    }
+};
