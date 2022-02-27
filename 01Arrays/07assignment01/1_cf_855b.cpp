a4fbadf1b5915bd676907180510af0bd6215cf3f
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-02-27 14:15:07 +0530

    arrays | revision

--- 01Arrays/07assignment01/1_cf_855b.cpp
+++ 01Arrays/07assignment01/1_cf_855b.cpp
@@ -3,26 +3,22 @@
 #include <climits>
 #include <vector>
 using namespace std;
+typedef long long int ll;
 
 int main() {
     int n;
     cin >> n;
-    vector<long long int> nums(n);
-    long long int p, q, r;
+    vector<ll> nums(n);
+    ll p, q, r;
     cin >> p >> q >> r;
-    long long int temp;
-    for (int i{0}; i < n; i++)
-        cin >> nums[i];
-    vector<long long int> preAr(n), sufAr(n);
+    for (int i{0}; i < n; i++) cin >> nums[i];
+    vector<ll> preAr(n), sufAr(n);
     preAr[0] = p * nums[0];
-    for (int i{1}; i < n; i++)
-        preAr[i] = max(p * nums[i], preAr[i - 1]);
+    for (int i{1}; i < n; i++) preAr[i] = max(p * nums[i], preAr[i - 1]);
     sufAr[n - 1] = r * nums[n - 1];
-    for (int i{n - 2}; i >= 0; i--)
-        sufAr[i] = max(sufAr[i + 1], r * nums[i]);
-    long long int ans = LLONG_MIN;
-    for (int i{0}; i < n; i++)
-        ans = max(ans, preAr[i] + q * nums[i] + sufAr[i]);
+    for (int i{n - 2}; i >= 0; i--) sufAr[i] = max(sufAr[i + 1], r * nums[i]);
+    ll ans = LLONG_MIN;
+    for (int i{0}; i < n; i++) ans = max(ans, preAr[i] + q * nums[i] + sufAr[i]);
     cout << ans << endl;
     return 0;
 }
