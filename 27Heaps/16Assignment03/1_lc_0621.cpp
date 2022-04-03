d352641e37d3da571ca4c591f3237b7a314b43a2
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-04-03 16:06:17 +0530

    heaps | task scheduling

--- 27Heaps/16Assignment03/1_lc_0621.cpp
+++ 27Heaps/16Assignment03/1_lc_0621.cpp
@@ -11,18 +11,18 @@ public:
         for (auto &ele : freq) if (ele) pq.push(ele);
         int ans = 0;
         while (pq.size()) {
-            int cnt = 0;
+            int cnt = 0;    // n+1 steps will be covered in [0, n]
             vector<int> tmp;
             while (cnt <= n) { // one cycle completed
-                if (pq.size() == 0) {
-                    if (tmp.size() > 0) ans += n-cnt+1; // ideal intervals
+                if (pq.size() == 0) { // priority queue got emptied in middle of a cycle
+                    if (tmp.size() > 0) ans += n-cnt+1; // but temp array still has elements: ideal intervals
                     break;
                 }
                 int x = pq.top();
                 pq.pop();
                 if (x-1 > 0) tmp.push_back(x-1);
-                ans++;
-                cnt++;
+                ans++;  // an interval of actual answer got executed
+                cnt++;  // an interval of current cycle got executed
             }
             for (auto &ele : tmp) pq.push(ele);
         }
