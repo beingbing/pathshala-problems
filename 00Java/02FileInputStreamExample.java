585e6c24634d57eb09df993b920ffd6572c5d02e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-03 19:40:51 +0530

    Java | refine explanation

--- 00Java/02FileInputStreamExample.java
+++ 00Java/02FileInputStreamExample.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 public class FileInputStreamExample {
     public static void main(String[] args) {
       // 1. establish a connection between file and program
-      // 2. read one character at a time without loading complete file in memory, similar
-      //    to if you were reading from a byte-array one element at a time
+      // 2. read one character at a time by making System read-call without loading 
+      //    complete file in memory, similar to if you were reading from a byte-array
+      //    one character at a time.
         try (FileInputStream fileInputStream = new FileInputStream("example.txt")) {
             int ch;
             while ((ch = fileInputStream.read()) != -1) {
