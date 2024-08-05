d602bb520797fd1c0a0acc499dc7d4083e05fd2e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-05 16:14:40 +0530

    Java | update BufferedOutputStream context

--- 00Java/02OutputStream/04BufferedOutputStreamExample.java
+++ 00Java/02OutputStream/04BufferedOutputStreamExample.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class BufferedOutputStreamExample {
     public static void main(String[] args) {
+        // To add buffering to an output stream, improving write performance by reducing the number of
+        // write operations to the underlying output stream
         try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("output.txt"))) {
             bos.write("Hello, World!".getBytes());
         } catch (IOException e) {
