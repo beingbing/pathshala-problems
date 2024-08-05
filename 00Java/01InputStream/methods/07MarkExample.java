7e07a9fbb32316691ab081414d8549d9a4434aac
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-05 20:54:07 +0530

    Java | practice InputStream mark(int readlimit) method

---
+++ 00Java/01InputStream/methods/07MarkExample.java
@@ -0,0 +1,22 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MarkExample {
+    public static void main(String[] args) {
+        byte[] data = "Hello, World!".getBytes();
+        try (InputStream is = new ByteArrayInputStream(data)) {
+            System.out.print((char) is.read()); // Read 'H'
+            is.mark(10); // Mark this position
+            for (int i = 0; i < 5; i++) {
+                System.out.print((char) is.read());
+            }
+            is.reset(); // Reset to marked position
+            for (int i = 0; i < 5; i++) {
+                System.out.print((char) is.read());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
