391eca684fc6af26d89c7db0c0a95c80f540b18a
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-10 17:08:36 +0530

    Java | practice flush() of OutputStream/Writer class

--- 00Java/01IOStreams/02OutputStream/methods/04FlushExample.java
+++ 00Java/01IOStreams/02OutputStream/methods/04FlushExample.java
@@ -3,6 +3,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+// Reason for Creation: it flushes the stream, ensuring that all buffered data is written to the
+// underlying device. It is crucial for writers that buffer data (e.g., BufferedWriter) to ensure
+// that data is not stuck in the buffer and is written out promptly.
+// Use Case: Ensuring that all data is written out, especially in scenarios where the writer is closed
+// immediately after writing, or when the data needs to be visible to readers before the stream is closed.
 public class FlushExample {
     public static void main(String[] args) {
       // Flushes the output stream and forces any buffered output bytes to be written out.
