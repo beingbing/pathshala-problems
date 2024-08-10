d38687455f1b3b2392a5c5e28ab3a49ffe711989
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-10 15:49:10 +0530

    Java | add description for PrintWriter

--- 00Java/01IOStreams/04Writer/08PrintWriterExample.java
+++ 00Java/01IOStreams/04Writer/08PrintWriterExample.java
@@ -2,6 +2,11 @@ import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// designed to provide methods for writing formatted text. It includes methods for 
+// printing primitive types, objects, and strings with optional automatic flushing.
+// It's a commonly used class for writing text data with a variety of formatting options.
+//  Writing formatted output, like logs, reports, or console output, where different
+// data types need to be written in a human-readable format.
 public class PrintWriterExample {
     public static void main(String[] args) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("output.txt"))) {
