420aeb8313340349c5f42772041ef346d9cda140
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-10 17:15:46 +0530

    Java | practice append() of Writer class along with variations

--- 00Java/01IOStreams/04Writer/methods/01AppendExample.java
+++ 00Java/01IOStreams/04Writer/methods/01AppendExample.java
@@ -7,6 +7,9 @@ import java.io.Writer;
 // more flexible and intuitive string concatenation.
 // Use Case: Appending strings or character sequences to a file or stream, useful 
 // for building text documents dynamically.
+// it has two more variations -
+// `Writer append(CharSequence csq, int start, int end)` - writer.append("Hello, Beautiful World!", 7, 16);  // Appends "Beautiful"
+// `Writer append(char c)` -  writer.append('A');
 public class AppendExample {
     public static void main(String[] args) {
         try (Writer writer = new FileWriter("output.txt", true)) {
