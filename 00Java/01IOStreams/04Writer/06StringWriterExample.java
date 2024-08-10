90b9dc2285d5482fb85fd063923a42dc90b04327
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-10 15:50:25 +0530

    Java | add description for Writer

--- 00Java/01IOStreams/04Writer/06StringWriterExample.java
+++ 00Java/01IOStreams/04Writer/06StringWriterExample.java
@@ -1,5 +1,7 @@
 import java.io.StringWriter;
 
+// Accumulating text data in memory, such as when constructing large strings dynamically,
+// building XML/JSON documents, or capturing formatted output for later use.
 public class StringWriterExample {
     public static void main(String[] args) {
         StringWriter writer = new StringWriter();
