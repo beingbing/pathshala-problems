af526b201827bf13fb9a255ee4c58586c6d15f52
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2024-08-10 15:53:01 +0530

    Java | add description for OutputStreamWriter

--- 00Java/01IOStreams/04Writer/04OutputStreamWriterExample.java
+++ 00Java/01IOStreams/04Writer/04OutputStreamWriterExample.java
@@ -2,6 +2,8 @@ import java.io.OutputStreamWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+// It is essential for writing text data to any output stream, like network connections or binary files,
+// where encoding is crucial, such as UTF-8 or ISO-8859-1.
 public class OutputStreamWriterExample {
     public static void main(String[] args) {
         try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("output.txt"), "UTF-8")) {
