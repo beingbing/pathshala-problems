8670f2749de49141b4f21b0be9701477c2ca3f58
Samar Shaikh <samar.shaikh@bharatpe.com>
2024-08-19 03:40:42 +0530

    Java | practice open/opens for reflection keywords

--- 00Java/03Modules/talks/module-info.java
+++ 00Java/03Modules/talks/module-info.java
@@ -1,8 +1,12 @@
+// open module zoo.animal.talks { // all exports are opened for reflection, then can't use `opens` inside anymore
 module zoo.animal.talks {
    // exports zoo.animal.talks.content to zoo.staff;
     exports zoo.animal.talks.content;
     exports zoo.animal.talks.media;
     exports zoo.animal.talks.schedule;
+
+    opens zoo.animal.talks.content to zoo.staff; // only staff can use reflection
+    opens zoo.animal.talks.schedule; // any requiring package can use reflection
 
     // requires zoo.animal.feeding;
     // requires zoo.animal.care;
